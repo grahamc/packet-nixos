f152a5d24aa3b41a605cdae0ce997a2c30cf0563
Graham Christensen <graham@grahamc.com>
2018-06-05 12:10:44 -0400

    Create an allwithtarball build separately, so we can test faster

--- all.nix
+++ all.nix
@@ -6,14 +6,31 @@ let
   all = pkgs.buildEnv {
     name = "all-pxe-images";
     paths = [ x86 arm ];
-  };
+};
+  allWithTarball = pkgs.runCommand "all-pxe-images-with-tarball" {} ''
+    now=$(date '+%Y-%m-%d--%H-%M-%S')
+    cp -r ${all} "./nixos-netboot-images-$now"
+    chmod u+w "./nixos-netboot-images-$now"
+    tar --create \
+        --verbose \
+        --bzip2 \
+        --dereference \
+        --file "./nixos-netboot-images-$now.tar.bz2" \
+        "./nixos-netboot-images-$now"
+
+    mv "./nixos-netboot-images-$now" $out
+    mv "./nixos-netboot-images-$now.tar.bz2" $out/
+'';
 in pkgs.runCommand "indexed-pxe-images" {} ''
 cp -r ${all} $out
 chmod u+w $out
+
 cd $out
-for f in $(find -L . -name 'netboot.ipxe' | grep -v '-' | sort -h); do
+
+for f in $(find -L . -name 'netboot.ipxe' -o -name '*.bz2' | sort -h); do
   printf '<li><a href="%s">%s</a></li>\n' \
     $f \
     $(basename $(dirname $f));
 done > $out/index.html
+
 ''
