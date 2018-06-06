86287b01d4e4046d10c0b1305b672e94a00636d1
Graham Christensen <graham@grahamc.com>
2018-06-06 08:51:14 -0400

    All pinned kernel versions up to 4.14

--- instances/m1.xlarge.x86/hardware.nix
+++ instances/m1.xlarge.x86/hardware.nix
@@ -2,7 +2,7 @@
 {
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.packageOverrides = pkgs: {
-    linux_4_9 = pkgs.linux_4_9.override {
+    linux_4_14 = pkgs.linux_4_14.override {
       extraConfig =
         ''
           MLX5_CORE_EN y
@@ -14,7 +14,7 @@
     "xhci_pci" "ehci_pci" "ahci" "megaraid_sas" "sd_mod"
   ];
 
-  boot.kernelPackages = pkgs.linuxPackages_4_9;
+  boot.kernelPackages = pkgs.linuxPackages_4_14;
   boot.kernelModules = [ "kvm-intel" ];
   boot.kernelParams =  [ "console=ttyS1,115200n8" ];
   boot.extraModulePackages = [ ];
