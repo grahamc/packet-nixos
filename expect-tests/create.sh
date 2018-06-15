3074cbac95a06386e6e5b17ea89518253331ad05
Graham Christensen <graham@grahamc.com>
2018-06-15 07:54:06 -0400

    Make the delete much more reliable

--- expect-tests/create.sh
+++ expect-tests/create.sh
@@ -35,7 +35,7 @@ function make_server() {
     REGION=$1
     PLAN="$2"
     URL="$3"
-    terminate=$(TZ="UTC" date --date='+2 hours' +'%Y-%m-%dT%H:%M:%S.000Z')
+    terminate=$(TZ=UTC date --date='+1 hour' --iso-8601=seconds)
     set -x
     curl -v --data '{
         "facility": "'"$REGION"'",
@@ -95,13 +95,23 @@ else
   region="$2"
 fi
 
+if [ "$name" == "c1.large.arm.xda" ]; then
+   pxe_url="$IPXE_ROOT/c1.large.arm/netboot.ipxe"
+#elif [ "$name" == "c2.medium.x86" ]; then
+#   pxe_url="http://147.75.197.111/c2med/netboot.ipxe"
+else
+   pxe_url="$IPXE_ROOT/$name/netboot.ipxe"
+fi
 
-url=$(make_server "$region" "$name" "$IPXE_ROOT/$name/netboot.ipxe")
+url=$(make_server "$region" "$name" "$pxe_url")
+
+cleanup() {
+          delete "$url"
+}
+trap cleanup EXIT
 
 while ! [ "$(fetch_info "$url" | wc -l)" -eq 5 ]; do sleep 1; done
 
 fetch_info "$url" | xargs ./run.sh
 
 sleep 10
-
-delete "$url"
