cea66a6319c09aa799efe326133a2d80331e9b67
Graham Christensen <graham@grahamc.com>
2018-06-16 16:30:52 -0400

    This is something

--- instance-types.nix
+++ instance-types.nix
@@ -5,8 +5,15 @@ in pkgs.writeText "pipeline.yml"
   (builtins.toJSON {
     steps = (builtins.map
       (x: {
-        command = "cd ./expect-tests; ./create.sh ${x.class}";
+        command = ''
+          export NIX_PATH=nixpkgs=${pkgs.path}
+          cd ./expect-tests
+          ./create.sh ${x.class}
+        '';
         label = "Testing image ${x.class}";
+        env = {
+          NIX_PATH = "nixpkgs=${pkgs.path}";
+        };
       })
       (builtins.attrValues filtered));
   }
