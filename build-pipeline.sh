ccaa0d6f87fd0e7eda24c063418da70c3c393c1b
Graham Christensen <graham@grahamc.com>
2018-10-18 09:08:27 -0400

    Unify the bild

--- build-pipeline.sh
+++ build-pipeline.sh
@@ -4,8 +4,5 @@ set -eux
 
 mkdir -p ./.buildkite
 
-STAGE="${1:-build}"
-
 nix-build ./instance-types.nix \
           --out-link ./.buildkite/pipeline.yml \
-          -A "$STAGE"
--- instance-types.nix
+++ instance-types.nix
@@ -5,44 +5,36 @@ let
   env = {
     NIX_PATH = "nixpkgs=${pkgs.path}";
   };
-in {
-  build = pkgs.writeText "pipeline.yml"
-    (builtins.toJSON {
-      steps = [{
-        command = ''
-          nix-build ./default.nix --keep-going
-          nix-build ./default.nix --keep-going
-          ./build-pipeline.sh upload
-        '';
-        label = "build";
-      }];
-    });
 
-  upload = pkgs.writeText "pipeline.yml"
-    (builtins.toJSON {
-      steps = [{
-        command = ''
-          echo "would upload :)"
-          exit 1
-          ./build-pipeline.sh launch
-        '';
-        label = "build";
-      }];
-    });
+  mkStep = label: command:
+    {
+      inherit label command env;
+    };
+  mkWaitStep = "wait";
+  mkBlockStep = label: {
+    block = label;
+  };
 
-  launch = pkgs.writeText "pipeline.yml"
+in
+  pkgs.writeText "pipeline.yml"
     (builtins.toJSON {
-      steps = (builtins.map
-        (x: {
-          command = ''
-            cd ./expect-tests
-            echo ". /etc/packet-nixos-config" > ./config.sh
-            ./create.sh ${x.class}
-          '';
-          label = "${x.class}";
-          inherit env;
-        })
-        (builtins.attrValues filtered));
+      steps = [
+        (mkStep "build-aarch64" "nix-build ./arm.nix --keep-going")
+        (mkStep "build-x86-64" "nix-build ./x8664.nix --keep-going")
+        mkWaitStep
+        (mkStep "upload" ''echo "would upload :)";'')
+        (mkBlockStep "Launch")
+        ] ++
+        (builtins.map
+          (x: {
+            command = ''
+              cd ./expect-tests
+              echo ". /etc/packet-nixos-config" > ./config.sh
+              ./create.sh ${x.class}
+            '';
+            label = "${x.class}";
+            inherit env;
+          })
+          (builtins.attrValues filtered));
     }
-  );
-}
+  )
