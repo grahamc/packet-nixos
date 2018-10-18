d5ff1af720599ca6966ce126a322730effbfb135
Graham Christensen <graham@grahamc.com>
2018-10-18 08:58:19 -0400

    Multi-staged buidl

--- build-pipeline.sh
+++ build-pipeline.sh
@@ -3,5 +3,9 @@
 set -eux
 
 mkdir -p ./.buildkite
+
+STAGE="${1:-build}"
+
 nix-build ./instance-types.nix \
-          --out-link ./.buildkite/pipeline.yml
+          --out-link ./.buildkite/pipeline.yml \
+          -A "$STAGE"
--- instance-types.nix
+++ instance-types.nix
@@ -1,20 +1,48 @@
 let
   pkgs = import ./nix {};
   filtered = import ./default.nix;
-in pkgs.writeText "pipeline.yml"
-  (builtins.toJSON {
-    steps = (builtins.map
-      (x: {
+
+  env = {
+    NIX_PATH = "nixpkgs=${pkgs.path}";
+  };
+in {
+  build = pkgs.writeText "pipeline.yml"
+    (builtins.toJSON {
+      steps = [{
         command = ''
-          cd ./expect-tests
-          echo ". /etc/packet-nixos-config" > ./config.sh
-          ./create.sh ${x.class}
+          nix-build ./default.nix --keep-going
+          nix-build ./default.nix --keep-going
+          ./build-pipeline.sh upload
         '';
-        label = "${x.class}";
-        env = {
-          NIX_PATH = "nixpkgs=${pkgs.path}";
-        };
-      })
-      (builtins.attrValues filtered));
-  }
-)
+        label = "build";
+      }];
+    });
+
+  upload = pkgs.writeText "pipeline.yml"
+    (builtins.toJSON {
+      steps = [{
+        command = ''
+          echo "would upload :)"
+          exit 1
+          ./build-pipeline.sh launch
+        '';
+        label = "build";
+      }];
+    });
+
+  launch = pkgs.writeText "pipeline.yml"
+    (builtins.toJSON {
+      steps = (builtins.map
+        (x: {
+          command = ''
+            cd ./expect-tests
+            echo ". /etc/packet-nixos-config" > ./config.sh
+            ./create.sh ${x.class}
+          '';
+          label = "${x.class}";
+          inherit env;
+        })
+        (builtins.attrValues filtered));
+    }
+  );
+}
