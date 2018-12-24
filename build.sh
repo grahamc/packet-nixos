13a1b5a6fa006ebf0b0d51a237fc7d076ee15918
Graham Christensen <graham@grahamc.com>
2018-12-23 19:38:17 -0500

    Make a build node with remote builders instead of a complicated build server

--- build.sh
+++ build.sh
@@ -5,33 +5,12 @@ set -o pipefail
 
 . ./expect-tests/config.sh
 
-buildHost=$BUILD_HOST_X86
+buildHost=root@$PXE_BUILD_HOST
 
-tmpDir=$(mktemp -t -d nixos-rebuild-packet-pxe.XXXXXX)
-SSHOPTS="${NIX_SSHOPTS:-} -A -o ControlMaster=auto -o ControlPath=$tmpDir/ssh-%n -o ControlPersist=60"
+set -x
 
-cleanup() {
-    for ctrl in "$tmpDir"/ssh-*; do
-        ssh -o ControlPath="$ctrl" -O exit dummyhost 2>/dev/null || true
-    done
-    rm -rf "$tmpDir"
-}
-trap cleanup EXIT
+drv=$(nix-instantiate ./all.nix --show-trace)
 
-set -eu
-
-if true; then
-    ssh $SSHOPTS "$BUILD_HOST_ARM" true
-    drv=$(nix-instantiate ./arm.nix --show-trace)
-    NIX_SSHOPTS=$SSHOPTS nix-copy-closure --to "$BUILD_HOST_ARM" "$drv"
-    out=$(ssh $SSHOPTS "$BUILD_HOST_ARM" NIX_REMOTE=daemon nix-store --realize "$drv" --keep-going --add-root /root/grahams-pxe-image --indirect)
-
-    ssh $SSHOPTS "$BUILD_HOST_ARM" NIX_REMOTE=daemon NIX_SSHOPTS="'-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'" nix-copy-closure --to "$buildHost" "$out"
-
-    drv=$(nix-instantiate ./all.nix --show-trace)
-else
-    drv=$(nix-instantiate ./x8664.nix --show-trace)
-fi
-
-NIX_SSHOPTS=$SSHOPTS nix-copy-closure --to "$buildHost" "$drv"
-ssh $SSHOPTS "$buildHost" NIX_REMOTE=daemon nix-store --realize "$drv" -j 8 --add-root /var/www/result --indirect --keep-going
+export NIX_SSHOPTS="-o UserKnownHostsFile=./pxe-known-host"
+nix-copy-closure --to "$buildHost" "$drv"
+ssh $NIX_SSHOPTS "$buildHost" NIX_REMOTE=daemon nix-store --realize "$drv" --add-root /var/www/result --indirect --keep-going
