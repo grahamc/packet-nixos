f9daa0bd5e2ed6ddabadb44dc9b8d4aa6b01541b
Graham Christensen <graham@grahamc.com>
2019-01-18 18:22:42 -0500

    alphabetize the check list, and add g2.large.x86

--- expect-tests/testall.sh
+++ expect-tests/testall.sh
@@ -19,15 +19,17 @@ check() {
     )&
 }
 
-check c1.small.x86
 check c1.large.arm
+check c1.small.x86
 check c1.xlarge.x86
 check c2.medium.x86
-check t1.small.x86
-check s1.large.x86
+check g2.large.x86
 check m1.xlarge.x86
 check m2.xlarge.x86
+check s1.large.x86
+check t1.small.x86
 check x1.small.x86
+check x2.xlarge.x86
 
 wait
 for i in $(seq 1 "$started"); do
